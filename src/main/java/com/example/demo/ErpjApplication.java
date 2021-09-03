4e44df2836d83fdfb7f94ab72d05fb6ee7bb73c9
kura84git <kou@kura84.com>
2021-09-04 01:35:25 +0900

    初期設定

---
+++ src/main/java/com/example/demo/ErpjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpjApplication.class, args);
+	}
+
+}

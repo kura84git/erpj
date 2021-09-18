c1793de936d3afdae59b351ad8f8e41029ca73ef
kura84git <kou@kura84.com>
2021-09-18 21:15:37 +0900

    ユーザマスタ登録処理

--- src/main/java/com/example/demo/controller/UserMasterController.java
+++ src/main/java/com/example/demo/controller/UserMasterController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.User;
-import com.example.demo.model.UserMasterForm;
+import com.example.demo.model.UserMasterRegForm;
+import com.example.demo.model.UserMasterSearchForm;
 import com.example.demo.service.UserMasterService;
 
 
@@ -26,22 +27,23 @@ public class UserMasterController {
 	UserMasterService userMasterService;
 	
 	@RequestMapping(value = {"/userMaster", "/userMasterSearch"}, method = RequestMethod.GET)
-    private ModelAndView getUserMaster(@ModelAttribute UserMasterForm form, HttpSession session, ModelAndView mv) {
+    private ModelAndView getUserMaster(@ModelAttribute UserMasterSearchForm form, HttpSession session, ModelAndView mv) {
 		String toUrl = "userMasterSearch";
 		
 		// 管理者ユーザでない場合
 		if (!session.getAttribute("authority").equals("ADMIN")) {
-			toUrl = "noAuthority";
+			mv.addObject("error", "権限がありません。");
+			toUrl = "failure";
 		}
 		
 		// 権限チェックボックス
-		mv.addObject("authority_checkbox", getAuthorityCheckBox());
+		mv.addObject("authority_checkbox", getAuthorityCheckbox());
 		
 		// 正規ユーザ／削除済ユーザチェックボックス
-		mv.addObject("live_del_checkbox", getLiveDelCheckBox());
+		mv.addObject("live_del_checkbox", getLiveDelCheckbox());
 		
 		// フォーム入力値
-		mv.addObject("userMasterForm", form);
+		mv.addObject("userMasterSearchForm", form);
 		
 		// ユーザ名をModelAndViewにセット
 		ControllerUtil.setUserNameToModelAndView(session, mv);
@@ -50,7 +52,7 @@ public class UserMasterController {
     }
 	
 	@RequestMapping(value = "/userMasterSearch", method = RequestMethod.POST)
-	private ModelAndView postUserMasterSearch(@ModelAttribute @Validated UserMasterForm form,
+	private ModelAndView postUserMasterSearch(@ModelAttribute @Validated UserMasterSearchForm form,
 			BindingResult result, HttpSession session, ModelAndView mv) {
 		
 		// エラーが発生した場合
@@ -74,13 +76,13 @@ public class UserMasterController {
 		}
 
 		// 権限チェックボックス
-		mv.addObject("authority_checkbox", getAuthorityCheckBox());
+		mv.addObject("authority_checkbox", getAuthorityCheckbox());
 		
 		// 正規ユーザ／削除済ユーザチェックボックス
-		mv.addObject("live_del_checkbox", getLiveDelCheckBox());
+		mv.addObject("live_del_checkbox", getLiveDelCheckbox());
 		
 		// フォーム入力値
-		mv.addObject("userMasterForm", form);
+		mv.addObject("userMasterSearchForm", form);
 		
 		// ユーザ名をModelAndViewにセット
 		ControllerUtil.setUserNameToModelAndView(session, mv);
@@ -88,25 +90,137 @@ public class UserMasterController {
 		return mv;	
 	}
 	
+	@RequestMapping(value = "userMasterReg", method = RequestMethod.GET)
+    private ModelAndView getUserMasterReg(@ModelAttribute UserMasterRegForm form, HttpSession session, ModelAndView mv) {
+		// 権限ラジオボタン
+		mv.addObject("authority_radiobutton", getAuthorityRadiobutton());
+		
+		// ラジオボタンの初期値セット
+		form.setAuthority_radiobutton(new String[] {"general"});
+		
+		// フォーム入力値
+		mv.addObject("userMasterRegForm", form);
+		
+		// ユーザ名をModelAndViewにセット
+		ControllerUtil.setUserNameToModelAndView(session, mv);
+		mv.setViewName("userMasterReg");
+        return mv;
+    }
+	
+	@RequestMapping(value = "/userMasterRegConfirm", method = RequestMethod.POST)
+	private ModelAndView postUserMasterRegConfirm(@ModelAttribute @Validated UserMasterRegForm form,
+			BindingResult result, HttpSession session, ModelAndView mv) {
+		String toUrl = "userMasterRegConfirm";
+		// エラーが発生した場合
+		if (result.hasErrors()) {
+			toUrl = "userMasterReg";
+			// 権限ラジオボタン
+			mv.addObject("authority_radiobutton", getAuthorityRadiobutton());
+		}
+		
+		// ユーザが既に存在する場合
+		if (userMasterService.existsUser(form.getUser_id())) {
+			toUrl = "userMasterReg";
+			mv.addObject("error_userExists", true);
+			// 権限ラジオボタン
+			mv.addObject("authority_radiobutton", getAuthorityRadiobutton());
+		}
+		
+		// フォーム入力値をセッションスコープにセット
+		session.setAttribute("userMasterRegForm", form);
+		
+		// フォーム入力値
+		mv.addObject("userMasterRegForm", form);
+		
+		// 一般ユーザを選択した場合
+		if (form.getAuthority_radiobutton()[0].equals("general")) {
+			mv.addObject("authority", "一般ユーザ");
+		// 管理者を選択した場合
+		} else {
+			mv.addObject("authority", "管理者");
+		}
+		
+		// ユーザ名をModelAndViewにセット
+		ControllerUtil.setUserNameToModelAndView(session, mv);
+		mv.setViewName(toUrl);
+		return mv;	
+	}
+	
+	@RequestMapping(value = "userMasterRegConfirm/cancel", method = RequestMethod.GET)
+    private ModelAndView getUserMasterRegConfirmCancel(@ModelAttribute UserMasterRegForm form, HttpSession session, ModelAndView mv) {
+		// 権限ラジオボタン
+		mv.addObject("authority_radiobutton", getAuthorityRadiobutton());
+		
+		// フォーム入力値をセッションスコープから取得
+		mv.addObject("userMasterRegForm", session.getAttribute("userMasterRegForm"));
+		
+		// ユーザ名をModelAndViewにセット
+		ControllerUtil.setUserNameToModelAndView(session, mv);
+		mv.setViewName("userMasterReg");
+        return mv;
+    }
+	
+	@RequestMapping(value = "/userMasterReg", method = RequestMethod.POST)
+	private ModelAndView postUserMasterReg(@ModelAttribute @Validated UserMasterRegForm form,
+			BindingResult result, HttpSession session, ModelAndView mv) {
+		String toUrl = "userMasterRegResult";
+		
+		// フォーム入力値をセッションスコープから取得
+		form = (UserMasterRegForm) session.getAttribute("userMasterRegForm");
+		
+		// ユーザを登録
+		if (userMasterService.insUser(form.getUser_id(), form.getUser_name(), form.getPassword(), form.getAuthority_radiobutton()) != 1) {
+			mv.addObject("error", "処理に失敗しました。");
+			toUrl = "failure";
+		}
+		
+		// フォーム入力値
+		mv.addObject("userMasterRegForm", form);
+		
+		// 一般ユーザを選択した場合
+		if (form.getAuthority_radiobutton()[0].equals("general")) {
+			mv.addObject("authority", "一般ユーザ");
+		// 管理者を選択した場合
+		} else {
+			mv.addObject("authority", "管理者");
+		}
+		
+		// ユーザ名をModelAndViewにセット
+		ControllerUtil.setUserNameToModelAndView(session, mv);
+		mv.setViewName(toUrl);
+		return mv;	
+	}
+	
 	/**
 	 * 権限チェックボックスを取得する
 	 * @return 権限チェックボックス
 	 */
-	private Map<String, String> getAuthorityCheckBox() {
-		Map<String, String> authorityCheckBox = new LinkedHashMap<>();
-		authorityCheckBox.put("general", "一般ユーザ");
-		authorityCheckBox.put("admin", "管理者");
-		return authorityCheckBox;
+	private Map<String, String> getAuthorityCheckbox() {
+		Map<String, String> authorityCheckbox = new LinkedHashMap<>();
+		authorityCheckbox.put("general", "一般ユーザ");
+		authorityCheckbox.put("admin", "管理者");
+		return authorityCheckbox;
+	}
+	
+	/**
+	 * 権限ラジオボタンを取得する
+	 * @return 権限チェックボックス
+	 */
+	private Map<String, String> getAuthorityRadiobutton() {
+		Map<String, String> authorityRadiobutton = new LinkedHashMap<>();
+		authorityRadiobutton.put("general", "一般ユーザ");
+		authorityRadiobutton.put("admin", "管理者");
+		return authorityRadiobutton;
 	}
 	
 	/**
 	 * 正規ユーザ／削除済ユーザチェックボックスを取得する
 	 * @return 正規ユーザ／削除済ユーザチェックボックス
 	 */
-	private Map<String, String> getLiveDelCheckBox() {
-		Map<String, String> authorityCheckBox = new LinkedHashMap<>();
-		authorityCheckBox.put("live", "正規ユーザ");
-		authorityCheckBox.put("del", "削除済ユーザ");
-		return authorityCheckBox;
+	private Map<String, String> getLiveDelCheckbox() {
+		Map<String, String> liveDelCheckbox = new LinkedHashMap<>();
+		liveDelCheckbox.put("live", "正規ユーザ");
+		liveDelCheckbox.put("del", "削除済ユーザ");
+		return liveDelCheckbox;
 	}
 }
